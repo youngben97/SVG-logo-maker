661c44ca09a67bad5ba7db26d3f8b2d294ba708c
GreatGaspargoo <b.c.young.97@gmail.com>
2023-09-02 20:30:36 -0600

    started init function. added helper function to shapes.js

--- index.js
+++ index.js
@@ -2,13 +2,37 @@
 const { readFile, writeFile } = require('fs/promises');
 const getInput = require('./lib/getInput.js');
 const generateSVG = require('./lib/generateSVG.js');
-const { Circle, Triangle, Square } = require('./lib/shapes.js');
+const { Circle, Triangle, Square, shapeSelect } = require('./lib/shapes.js');
 
 
 function init() {
-    getInput().then((response) =>
-        console.log(response)
-    );
-}
+getInput()
+    .then((response) => shapeSelect(response))
+    .then((selectedShape) => {
+        console.log(selectedShape)
+    })
+};
+
 
 init();
+
+// getInput()
+//     .then((response) => shapeSelect(response))
+//     .then((selectedShape) => {
+//         console.log(selectedShape)
+//     })
+
+//getInput()
+// .then((response) => {
+//     const parsedResponse = JSON.parse(response);
+//     return shapeSelect(parsedResponse);
+// })
+// .then((selectedShape) => {
+//     console.log(selectedShape);
+// })
+
+// getInput()
+// .then((response) => {
+//     const selectedShape = shapeSelect(response);
+//     console.log(selectedShape);
+// })
