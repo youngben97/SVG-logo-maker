9c6ba549c89f3d6cc2fb0d164559edeed0e8b5e8
GreatGaspargoo <b.c.young.97@gmail.com>
2023-09-02 18:37:02 -0600

    added to shape classes and tests

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
-//Packages
-const inquirer = require('inquirer');
+//Packages/Modules
+const getInput = require('./lib/getInput.js')
+const { Circle, Triangle, Square } = require('./lib/shapes.js');
 
-//Inquirer
-function getInput() {
-    return inquirer.prompt([
-    {
-        name: 'text',
-        message: 'Enter up to three characters for your logo',
-        type: 'input',
-        validate: (text) => {
-            if(!text.length) {
-                return 'No input detected. Enter up to three characters.';
-            } else if (text.length > 3) {
-                return 'Enter no more than three characters.';
-            } else {
-                return true;
-            }
-        }
-    },
-    {
-        name: 'textColor',
-        message: 'Enter a color for the logo text (color name or hexadecimal number)',
-        type: 'input'
-    },
-    {
-        name: 'shape',
-        message: 'Select a shape for the logo',
-        type: 'list',
-        choices: [
-            "Circle",
-            "Triangle",
-            "Square"
-        ]
-    },
-    {
-        name: 'shapeColor',
-        message: 'Enter a color for the logo shape (color name or hexadecimal number)',
-        type: 'input'
-    }
-]);
-};
 
 function init() {
     getInput().then((response) =>
