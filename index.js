40306b463c9d58bc3164d5d33902cf5fab7ea2a9
GreatGaspargoo <b.c.young.97@gmail.com>
2023-09-05 20:36:25 -0600

    commented code. next record video and add README

--- index.js
+++ index.js
@@ -3,36 +3,14 @@ const getInput = require('./lib/getInput.js');
 const { generateSVG, writeSVG } = require('./lib/generateSVG.js');
 const { shapeSelect } = require('./lib/shapes.js');
 
-
+//Initialize app
 function init() {
 getInput()
     .then((response) => shapeSelect(response))
     .then((selectedShape) => {
-        writeSVG('./examples/example.svg', generateSVG(selectedShape))
+        writeSVG('./examples/logo.svg', generateSVG(selectedShape))
     })
 };
 
-//next you need to pass selected shape to generate svg and then pass that to writeFile
-
+//Call function
 init();
-
-// getInput()
-//     .then((response) => shapeSelect(response))
-//     .then((selectedShape) => {
-//         console.log(selectedShape)
-//     })
-
-//getInput()
-// .then((response) => {
-//     const parsedResponse = JSON.parse(response);
-//     return shapeSelect(parsedResponse);
-// })
-// .then((selectedShape) => {
-//     console.log(selectedShape);
-// })
-
-// getInput()
-// .then((response) => {
-//     const selectedShape = shapeSelect(response);
-//     console.log(selectedShape);
-// })
